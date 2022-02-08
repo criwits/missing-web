8f9cb5fe1f1259ae9bf898742b2a3c1a7691d149
criwits <hanswan@tom.com>
2022-02-08 13:13:57 +0800

    Script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-mv ./_book/.git ./
+
 gitbook build
-mv ./.git ./_book
-cd _book
+git add .
+git commit -m $1
+git push origin main
+
+git checkout gh-pages
+rm -rf `ls | egrep -v '(.git|_book|node_modules)'`
+mv ./_book/* ./
+
+git add .
+git commit -m "Publish book"
+git push origin gh-pages
+
+git checkout main
