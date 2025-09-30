5335d903d6a0bd6fca9093bf1dd3403c117dceef
criwits <wanqichen@outlook.com>
2025-09-30 11:56:56 +0800

    添加图片点击放大

--- utils/create_image_renderer.py
+++ utils/create_image_renderer.py
@@ -13,6 +13,7 @@ with open("layouts/_default/_markup/render-image.html", "w") as f:
 {{- $alt := .Text | default "" -}}
 {{- $fileName := replaceRE "([?#].*)$" "" $src | path.Base -}}
 {{- $result := first 1 (where .Page.Resources "Name" $fileName) -}}
+<a data-fancybox data-src="{{ $srcStatic }}">
 {{- with $result -}}
     {{- $r := index . 0 -}}
     {{- if eq $r.MediaType.SubType "svg" -}}
@@ -23,6 +24,7 @@ with open("layouts/_default/_markup/render-image.html", "w") as f:
 {{- else -}}
     <img src="{{ $srcStatic }}" alt="{{ $alt }}" loading="lazy" />
 {{- end -}}
+</a>
             """)
     
 print("render-image.html 文件已创建。")
